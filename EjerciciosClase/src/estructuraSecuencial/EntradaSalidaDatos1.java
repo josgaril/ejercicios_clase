8385581d7effd8c583f25e0f30915a1cfe91d0eb
iñaki alvira <i.alvira.urrutia@gmail.com>
2019-12-10 12:29:21 +0100

    Completado Ejercicio10 EntradaSalidaDatos3

--- EjerciciosClase/src/estructuraSecuencial/EntradaSalidaDatos1.java
+++ EjerciciosClase/src/estructuraSecuencial/EntradaSalidaDatos1.java
@@ -8,8 +8,11 @@ import java.util.Scanner;
 public class EntradaSalidaDatos1 {
 
 	public static void main(String[] args) throws IOException {
-		//Programa Java que lea dos números enteros por teclado y los muestre por pantalla.
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		/*
+		 * 1- Programa Java que lea dos números enteros por teclado y los muestre por //
+		 * pantalla.
+		 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("Introduzca el primer número: ");
 		String numero1 = br.readLine();
 		System.out.print("Introduzca el segundo número: ");
@@ -17,31 +20,55 @@ public class EntradaSalidaDatos1 {
 		System.out.println("Primer número: " + numero1);
 		System.out.println("Segundo numero: " + numero2);
 		System.out.println("");
-		
-		//  Programa Java que lea un nombre y muestre por pantalla:
-		//  “Buenos dias nombre_introducido”
+
+		// Con Scanner
+		int n1, n2;
+		Scanner sc = new Scanner(System.in);
+		// leer el primer número
+		System.out.println("Introduce un número entero: ");
+		n1 = sc.nextInt(); // lee un entero por teclado
+		// leer el segundo número
+		System.out.println("Introduce otro número entero: ");
+		n2 = sc.nextInt(); // lee un entero por teclado
+		// mostrar resultado
+		System.out.println("Ha introducido los números: " + n1 + " y " + n2);
+		System.out.println("");
+
+		/*
+		 *  2- Programa Java que lea un nombre y muestre por pantalla: “Buenos dias //
+		 * nombre_introducido”
+		 */		
 		System.out.print("Introduzca su nombre: ");
-		String nombre= br.readLine();
+		String nombre = br.readLine();
 		System.out.println("Buenos días " + nombre);
 		System.out.println("");
-		
-		//programa Java que lee un número entero por teclado y obtiene y muestra por pantalla el doble y el triple de ese número
-		
-		  System.out.print("Introduzca un número para calcular su doble y su triple: ");
-		  int numero = Integer.parseInt(br.readLine());
-		  System.out.println("El doble de " + numero  + " es: " + (2*numero)); 
-		  System.out.println("El triple de " + numero  + " es: " + (3*numero));
-		  System.out.println("");
-		 
-		
-		//otra forma
-		  Scanner sc = new Scanner(System.in);
-	        int numero3;
-	        System.out.print("Introduce un número entero:");
-	        numero3 = sc.nextInt();
-	        System.out.println("Número introducido: " + numero3);
-	        System.out.println("Doble de " + numero3 + " -> "+ 2*numero3);
-	        System.out.println("Triple de " + numero3 + " -> "+ 3*numero3);
+
+		// Con Scanner
+		Scanner sc1 = new Scanner(System.in);
+		String cadena;
+		System.out.println("Introduce un nombre: ");
+		cadena = sc1.nextLine();
+		System.out.println("Buenos Días " + cadena);
+		System.out.println("");
+
+		/*
+		 * 3- Programa Java que lee un número entero por teclado y obtiene y muestra por
+		 * pantalla el doble y el triple de ese número
+		 */
+		System.out.print("Introduzca un número para calcular su doble y su triple: ");
+		int numero = Integer.parseInt(br.readLine());
+		System.out.println("El doble de " + numero + " es: " + (2 * numero));
+		System.out.println("El triple de " + numero + " es: " + (3 * numero));
+		System.out.println("");
+
+		// Con Scanner
+		Scanner sc2 = new Scanner(System.in);
+		int numero3;
+		System.out.print("Introduce un número entero:");
+		numero3 = sc2.nextInt();
+		System.out.println("Número introducido: " + numero3);
+		System.out.println("Doble de " + numero3 + " -> " + 2 * numero3);
+		System.out.println("Triple de " + numero3 + " -> " + 3 * numero3);
 	}
 
 }
--- EjerciciosClase/src/estructuraSecuencial/EntradaSalidaDatos3.java
+++ EjerciciosClase/src/estructuraSecuencial/EntradaSalidaDatos3.java
@@ -1,17 +1,82 @@
 package estructuraSecuencial;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class EntradaSalidaDatos3 {
 
-	public static void main(String[] args) {
-		//Programa que tome como dato de entrada un número que corresponde a la longitud del radio una esfera 
-		// y nos calcula y escribe el volumen de la esfera que se corresponden con dicho radio.
-		//v = (4/3)*PI*r^3
-				
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		/*
+		 * 8 Programa que tome como dato de entrada un número que corresponde a la //
+		 * longitud del radio una esfera y nos calcula y escribe el volumen de la esfera
+		 * que se corresponden con dicho radio.
+		 */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print(("Introduzca el radio de la esfera: "));
+		double radioEsfera = Double.parseDouble(br.readLine());
+		System.out.println("El volúmen de la esfera es: " + (4.0 / 3) * Math.PI * Math.pow(radioEsfera, 3));
+		System.out.println("");
+
+		// Con Scanner
+		Scanner sc8 = new Scanner(System.in);
+		double radio, volumen;
+		System.out.print("Introduzca radio de la esfera: ");
+		radio = sc8.nextDouble();
+		System.out.println("Volumen de la esfera de radio " + radio + " = " + (4.0 / 3) * Math.PI * Math.pow(radio, 3));
+		System.out.println("");
+
+		/*
+		 * 9- Programa Java que calcule el área de un triángulo en función de las
+		 * longitudes de sus lados (a, b, c) Area = RaizCuadrada(p*(p-a)*(p-b)*(p-c))
+		 * donde p = (a+b+c)/2 Para calcular la raíz cuadrada se utiliza el método
+		 * Math.sqrt()
+		 */
+		System.out.println("Introduzca la longitud de los lados del triángulo:");
+		System.out.print("A--> ");
+		double A = Double.parseDouble(br.readLine());
+		System.out.print("B-> ");
+		double B = Double.parseDouble(br.readLine());
+		System.out.print("C-> ");
+		double C = Double.parseDouble(br.readLine());
+		double p = (A + B + C) / 2;
+		System.out.println("El área del triángulo es: " + Math.sqrt(p * (p - A) * (p - B) * (p - C)));
+		System.out.println("");
+
+		// Con scanner
+		Scanner sc9 = new Scanner(System.in);
+		System.out.print("A-> ");
+		double A1 = sc9.nextDouble();
+		System.out.print("B-> ");
+		double B1 = sc9.nextDouble();
+		System.out.print("C-> ");
+		double C1 = sc9.nextDouble();
+		double p9 = (A + B + C) / 2;
+		System.out.print("El área del triángulo es: " + Math.sqrt(p9 * (p9 - A1) * (p9 - B1) * (p9 - C1)));
+		System.out.println("");
+
+		/*
+		 * 10- Programa Java que lea un número entero de 3 cifras y muestre por separado
+		 * las cifras del número
+		 */
+		System.out.print("Introduzca un numero entero para mostrar sus cifras separadas: ");
+		int NUM = Integer.parseInt(br.readLine());
+		System.out.println("Primera cifra: " + NUM/100);
+		System.out.println("Segunda cifra: " + (NUM/10)%10);
+		System.out.println("Tercera cifra: " + NUM%10);
+		System.out.println("");
+		
+		//COn Scann
+		System.out.print("Introduzca un numero entero para mostrar sus cifras separadas: ");
+		Scanner sc10 = new Scanner(System.in);
+		int NUM10 = sc10.nextInt();
+		System.out.println("Primera cifra: " + NUM10/100);
+		System.out.println("Segunda cifra: " + (NUM10/10)%10);
+		System.out.println("Tercera cifra: " + NUM10%10);
+		System.out.println("");
+		
+		
 		
 	}
 
