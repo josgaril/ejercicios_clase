d5d271231a87b6fac790d83195f1ac217acb432b
iñaki alvira <i.alvira.urrutia@gmail.com>
2019-12-13 10:45:18 +0100

    creado paquete ficheros y directorios

---
+++ EjerciciosClase/src/ficherosyDirectorios/UnidadesDelDisco.java
@@ -0,0 +1,15 @@
+package ficherosyDirectorios;
+
+public class UnidadesDelDisco {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 1. Programa que muestre todas las unidades de disco del ordenador indicando
+		 * para cada una de ellas el tamaño disponible y el tamaño total. Clase File.
+		 */
+		
+		
+	}
+
+}
