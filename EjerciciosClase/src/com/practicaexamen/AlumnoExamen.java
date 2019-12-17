fe33b8673dcac86c542d8777f1dc163cecab0277
iñaki alvira <i.alvira.urrutia@gmail.com>
2019-12-17 14:28:48 +0100

    Empezando prueba paquete practicaexamen

---
+++ EjerciciosClase/src/com/practicaexamen/AlumnoExamen.java
@@ -0,0 +1,15 @@
+package com.practicaexamen;
+
+import java.util.Date;
+
+public class AlumnoExamen {
+
+	private Long id;
+	private String nombre;
+	private String apellidos;
+	private String sexo;
+	private String dni;
+	private Date fechaNacimiento;
+	
+	
+}
