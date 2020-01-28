cf50e59fea78a6d3164b63f79129445ffc4a5bc8
iñaki alvira <i.alvira.urrutia@gmail.com>
2020-01-28 14:32:49 +0100

    opciones de listar,agregar,eliminar y modificar sin menu todavia

--- UF2216-MySQL/src/com/ipartek/formacion/uf2216/presentacion/PresentacionConsola.java
+++ UF2216-MySQL/src/com/ipartek/formacion/uf2216/presentacion/PresentacionConsola.java
@@ -1,5 +1,11 @@
 package com.ipartek.formacion.uf2216.presentacion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -10,30 +16,130 @@ public class PresentacionConsola {
 	private final static TreeMap<Long, Libro> libros = new TreeMap<>();
 	private final static Scanner scanner = new Scanner(System.in);
 	private static long ultimoId = 1L;
-	
-	public static void main(String[] args) {
+	private static final String usuario = "root";
+	private static final String password = "admin";
+	private static final String URL = "jdbc:mysql://localhost:3306/uf2213?serverTimezone=UTC"; // Cadena de conexiÃ³n de
+																								// MySQL
+					
+	public static void main(String[] args) throws SQLException {
+																			
 		boolean esNuevoRegistro;
 					
 		do {
-			Libro libro = pedirLibro();
 			
-			mostrarLibro(libro);
+			System.out.println("Conectando");
 
-			guardarLibro(libro);
+			Connection con = DriverManager.getConnection(URL, usuario, password);
+
+			System.out.println("Conectado");
+
+			System.out.println("LISTADO DE LIBROS");
+
+			String sql = "SELECT * FROM libros";
+
+			Statement s = con.createStatement();
+
+			ResultSet rs = s.executeQuery(sql);
+
+			while (rs.next()) {
+				System.out.println(rs.getString("titulo") + "\t" + rs.getString("isbn") + "\t" + rs.getInt("numeroPaginas") + "\t" + rs.getString("formato"));
+			}
 			
-			listadoLibros();
+			
+			System.out.println("AGREGAR LIBROS");
+			
+			String titulo = "La montaÃ±a";
+			String isbn = "1234567890";
+			String numeroPaginas = "111";
+			String formato = "digital";
+			
+			sql = "INSERT INTO libros (titulo, isbn, numeroPaginas,formato) VALUES (?,?,?,?) ";
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			ps.setString(1, titulo);
+			ps.setString(2, isbn);
+			ps.setString(3, numeroPaginas);
+			ps.setString(4, formato);
+			
+			int numeroRegistrosModificados = ps.executeUpdate();
+			
+			if(numeroRegistrosModificados == 1) {
+				System.out.println("Insertado correctamente");
+			} else {
+				System.err.println("No se ha insertado correctamente");
+				System.err.println(numeroRegistrosModificados);
+			}
+			
+			System.out.println("MODIFICAR LIBRO");
+			Long idAModificar = 3L;
+			titulo = "El sendero";
+			isbn = "0987654321";
+			numeroPaginas = "66";
+			formato = "papel";
+			
+			sql = "UPDATE usuarios SET titulo = ?, isbn = ?, numeroPaginas = ?, formato = ? WHERE id = ?";
+			
+			ps = con.prepareStatement(sql);
+			
+			ps.setString(1, titulo);
+			ps.setString(2, isbn);
+			ps.setString(3, numeroPaginas);
+			ps.setString(4, formato);
+			ps.setLong(5, idAModificar);
+		
+			numeroRegistrosModificados = ps.executeUpdate();
+			
+			if(numeroRegistrosModificados == 1) {
+				System.out.println("Libro modificado correctamente");
+			} else {
+				System.err.println("No se ha modificado correctamente");
+				System.err.println(numeroRegistrosModificados);
+			}
+			
+			/*
+			 * System.out.println("ELIMINAR LIBRO"); Long id = 2L; sql =
+			 * "DELETE FROM libros WHERE id = ?";
+			 * 
+			 * ps = con.prepareStatement(sql);
+			 * 
+			 * ps.setLong(1, id); numeroRegistrosModificados = ps.executeUpdate();
+			 * 
+			 * if(numeroRegistrosModificados == 1) {
+			 * System.out.println("El libro se ha borrado correctamente"); } else {
+			 * System.err.println("No se ha borrado correctamente");
+			 * System.err.println(numeroRegistrosModificados); }
+			 */
+			
+			ps.close();
+			rs.close();
+			s.close();
+			con.close();
+			/*
+			 * listadoLibros();
+			 * 
+			 * Libro libro = pedirLibro();
+			 * 
+			 * mostrarLibro(libro);
+			 * 
+			 * guardarLibro(libro);
+			 */
+			
+			
 			
 			//Preguntar si el usuario quiere introducir un nuevo registro
-			System.out.print("¿Desea introducir un nuevo registro? (S/n) ");
+			System.out.print("ï¿½Desea introducir un nuevo registro? (S/n) ");
 			esNuevoRegistro = !scanner.nextLine().equalsIgnoreCase("n");
 		} while (esNuevoRegistro);
 		
-		System.out.println("Saliendo de la aplicación. Gracias por usar esta aplicación");
+		System.out.println("Saliendo de la aplicaciï¿½n. Gracias por usar esta aplicaciï¿½n");
 		
 		scanner.close();
 	}
 
 	private static Libro pedirLibro() {
+		
+		
+		
 		//Pedir datos de libro
 		String titulo, isbn;
 		int numeroPaginas = -1;
@@ -44,18 +150,18 @@ public class PresentacionConsola {
 		numeroPaginas = pedirNumeroPaginas(numeroPaginas);
 		formato = pedirFormato(formato);
 		
-		//Creación de libro en base a los datos introducidos
+		//Creaciï¿½n de libro en base a los datos introducidos
 		Libro libro = new Libro(ultimoId, titulo, isbn, numeroPaginas, formato);
 		
 		return libro;
 	}
 
 	private static void guardarLibro(Libro libro) {
-		System.out.print("¿Quieres guardar este libro? (S/n) ");
+		System.out.print("ï¿½Quieres guardar este libro? (S/n) ");
 		
-		//Si me dicen que sí, guardo el libro
+		//Si me dicen que sï¿½, guardo el libro
 		if(!scanner.nextLine().equalsIgnoreCase("n")) {
-			//Añadir el libro a la colección de libros
+			//Aï¿½adir el libro a la colecciï¿½n de libros
 			libros.put(ultimoId++, libro);
 		}
 	}
@@ -64,13 +170,13 @@ public class PresentacionConsola {
 		System.out.println("LISTADO DE LIBROS");
 		
 		System.out.println(
-				"Id\tISBN\t\tNúmero de páginas\tFormato\tTítulo");
+				"Id\tISBN\t\tNï¿½mero de pï¿½ginas\tFormato\tTï¿½tulo");
 		for (Libro libroExtraido : libros.values()) {
 			System.out.println(
 					libroExtraido.getId() +
 					"\t" + libroExtraido.getIsbn() +
 					"\t" + libroExtraido.getNumeroPaginas() +
-					"\t\t\t" + (libroExtraido.isFormato() ? "Digital" : "Papel") +
+					"\t\t" + (libroExtraido.isFormato() ? "Digital" : "Papel") +
 					"\t" + libroExtraido.getTitulo());
 		}
 	}
@@ -78,9 +184,9 @@ public class PresentacionConsola {
 	private static void mostrarLibro(Libro libro) {
 		System.out.println(
 				"Id: " + libro.getId() + 
-				"\nTítulo: " + libro.getTitulo() + 
+				"\nTï¿½tulo: " + libro.getTitulo() + 
 				"\nISBN: " + libro.getIsbn() + 
-				"\nNúmero de páginas: " + libro.getNumeroPaginas() +
+				"\nNï¿½mero de pï¿½ginas: " + libro.getNumeroPaginas() +
 				"\nFormato: " + (libro.isFormato() ? "Digital" : "Papel"));
 	}
 
@@ -88,7 +194,7 @@ public class PresentacionConsola {
 		boolean esFormatoCorrecto;
 		String stringFormato;
 		do {
-			//Introducción de formato
+			//Introducciï¿½n de formato
 			System.out.print("Formato (digital o papel): ");
 			stringFormato = scanner.nextLine().trim();
 			
@@ -106,8 +212,8 @@ public class PresentacionConsola {
 	private static int pedirNumeroPaginas(int numeroPaginas) {
 		boolean esNumeroPaginasCorrecto;
 		do {
-			//Introducción de número de páginas
-			System.out.print("Número de páginas: ");
+			//Introducciï¿½n de nï¿½mero de pï¿½ginas
+			System.out.print("Nï¿½mero de pï¿½ginas: ");
 			
 			try {
 				numeroPaginas = scanner.nextInt();
@@ -116,13 +222,13 @@ public class PresentacionConsola {
 					esNumeroPaginasCorrecto = true;
 				} else {
 					esNumeroPaginasCorrecto = false;
-					System.out.println("El número de páginas debe ser mínimo 1");
+					System.out.println("El nï¿½mero de pï¿½ginas debe ser mï¿½nimo 1");
 				}
 			} catch (Exception e) {
-				System.out.println("Debes introducir un número (sólo dígitos)");
+				System.out.println("Debes introducir un nï¿½mero (sï¿½lo dï¿½gitos)");
 				esNumeroPaginasCorrecto = false;
 			} finally {
-				//Consumir el enter si ha ido bien, o el texto si ha habido una excepción
+				//Consumir el enter si ha ido bien, o el texto si ha habido una excepciï¿½n
 				scanner.nextLine();
 			}
 		} while (!esNumeroPaginasCorrecto);
@@ -133,7 +239,7 @@ public class PresentacionConsola {
 		boolean esIsbnCorrecto;
 		String isbn;
 		do {
-			//Introducción de ISBN
+			//Introducciï¿½n de ISBN
 			System.out.print("ISBN: ");
 			isbn = scanner.nextLine();
 			
@@ -141,7 +247,7 @@ public class PresentacionConsola {
 				esIsbnCorrecto = true;
 			} else {
 				esIsbnCorrecto = false;
-				System.out.println("El ISBN debe ser un número de longitud 10");
+				System.out.println("El ISBN debe ser un nï¿½mero de longitud 10");
 			}
 		} while (!esIsbnCorrecto);
 		return isbn;
@@ -151,17 +257,17 @@ public class PresentacionConsola {
 		boolean esTituloCorrecto;
 		String titulo;
 		do {
-			//Introducción de título
-			System.out.print("Título: ");
+			//Introducciï¿½n de tï¿½tulo
+			System.out.print("Tï¿½tulo: ");
 			titulo = scanner.nextLine().trim();
 			
-			//Validación del título
+			//Validaciï¿½n del tï¿½tulo
 			if (titulo.length() > 3 && titulo.length() <= 150) {
 				esTituloCorrecto = true;
 				
 			} else {
 				esTituloCorrecto = false;
-				System.out.println("El título debe tener un tamaño mínimo de 3 letras y máximo de 150");
+				System.out.println("El tï¿½tulo debe tener un tamaï¿½o mï¿½nimo de 3 letras y mï¿½ximo de 150");
 			}
 		} while (!esTituloCorrecto);
 		return titulo;
