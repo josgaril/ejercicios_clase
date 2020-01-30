9f7d78463bbaf60f9069bfd69ea9114a295303d5
iñaki alvira <i.alvira.urrutia@gmail.com>
2020-01-30 12:36:09 +0100

    comnpletado presencation por consola con BD libros

--- UF2216-MySQL/src/com/ipartek/formacion/uf2216/presentacion/PresentacionConsola.java
+++ UF2216-MySQL/src/com/ipartek/formacion/uf2216/presentacion/PresentacionConsola.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.Statement;
 
 public class PresentacionConsola {
 
@@ -16,15 +16,14 @@ public class PresentacionConsola {
 	 */
 	private static final String usuario = "root";
 	private static final String password = "admin";
-	private static final String URL = "jdbc:mysql://localhost:3306/uf2213?serverTimezone=UTC";//Cadena de conexión de MySQL
+	private static final String URL = "jdbc:mysql://localhost:3306/uf2213?serverTimezone=UTC";// Cadena de conexión de
+																								// MySQL
 	private static final int OPCION_LISTADO = 1;
 	private static final int OPCION_OBTENERPORID = 2;
 	private static final int OPCION_AGREGAR = 3;
 	private static final int OPCION_MODIFICAR = 4;
 	private static final int OPCION_ELIMINAR = 5;
 	private static final int OPCION_SALIR = 0;
-
-	private static Scanner s = new Scanner(System.in);
 
 	public static void main(String[] args) throws SQLException {
 
@@ -84,109 +83,125 @@ public class PresentacionConsola {
 		}
 	}
 
-	private static void listado() {
+	private static Connection getConnection() {
 		try {
-			System.out.println("Conectando para Listar todos los libros");
-			Connection con = DriverManager.getConnection(URL, usuario, password);
+			return DriverManager.getConnection(URL, usuario, password);
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Error en la coneción a la base de datos", e);
+		}
+	}
+
+	private static void listado() {
+		System.out.println("Conectando para Listar todos los libros");
+		try (Connection con = getConnection()) {
 			System.out.println("Conectado");
 			System.out.println("LISTADO");
 			String sql = "SELECT * FROM libros";
-			PreparedStatement ps = con.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				 System.out.println(rs.getLong("id") + "\t" + rs.getString("titulo") + "\t" + rs.getString("isbn") + "\t"
-						+ rs.getInt("numeroPaginas") + "\t" + rs.getString("formato"));
+			try (Statement s = con.createStatement()) {
+				try (ResultSet rs = s.executeQuery(sql)) {
+					while (rs.next()) {
+						System.out
+								.println(rs.getLong("id") + "\t" + rs.getString("titulo") + "\t" + rs.getString("isbn")
+										+ "\t" + rs.getInt("numeroPaginas") + "\t" + rs.getBoolean("formato"));
+					}
+				}
 			}
+
 		} catch (SQLException e) {
 			throw new AccesoDatosException("No se ha podido realizar el listado de libros", e);
 		}
 	}
 
 	private static void getbyid() {
-		try {
-			System.out.println("Conectando para obtener por ID");
-			Connection con = DriverManager.getConnection(URL, usuario, password);
+		System.out.println("Conectando para obtener por ID");
+		try (Connection con = getConnection();) {
 			System.out.println("Conectado");
 			System.out.println("OBTENER POR ID");
-			Long id = 1L;
+			Long id = 14L;
 			String sql = "SELECT * FROM libros WHERE id = ?";
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setLong(1, id);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				System.out.println((rs.getString("titulo") + "\t" + rs.getString("isbn") + "\t"
-						+ rs.getString("numeroPaginas") + "\t" + rs.getString("formato")));
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
+
+				ps.setLong(1, id);
+				try (ResultSet rs = ps.executeQuery()) {
+
+					if (rs.next()) {
+						System.out
+								.println(rs.getLong("id") + "\t" + rs.getString("titulo") + "\t" + rs.getString("isbn")
+										+ "\t" + rs.getInt("numeroPaginas") + "\t" + rs.getBoolean("formato"));
+					} else {
+						System.out.println("No encontrado");
+					}
+				}
 			}
 		} catch (SQLException e) {
 			throw new AccesoDatosException("No se ha podido obtener el libro pod id", e);
 		}
-		// TODO ACABAR METODO GETBYID CON TRY/CATH
 	}
 
 	private static void insert() {
 
 		System.out.println("Conectando para agregar");
-		try  {
-			Connection con = DriverManager.getConnection(URL, usuario, password);
+		try (Connection con = getConnection()) {
+
 			System.out.println("Conectado");
 			System.out.println("Agregar libro");
-			String titulo = "Los del alla";
-			String isbn = "1234561230";
-			String numeroPaginas = "35";
-			String formato = "digital";
-			System.out.println("Agregar libro1");
+			String titulo = "yiiieso";
+			String isbn = "1111111111";
+			Integer numeroPaginas = 11;
+			Boolean formato = false;
 
-			String sql = "INSERT INTO libros (titulo, isbn, numeroPaginas,formato) VALUES(?,?,?,?,?)";
-			System.out.println("Agregar libro2");
+			String sql = "INSERT INTO libros (titulo, isbn, numeroPaginas,formato) VALUES(?,?,?,?)";
 
-			PreparedStatement ps = con.prepareStatement(sql);
-			System.out.println("Agregar libro21");
-			ps.setString(1, titulo);
-			ps.setString(2, isbn);
-			ps.setString(3, numeroPaginas);
-			ps.setString(4, formato);
-			System.out.println("Agregar libro22");
-			int numeroRegistrosModificados = ps.executeUpdate();
-			System.out.println("Agregar libro3");
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
 
-			if (numeroRegistrosModificados == 1) {
-				System.out.println("Insertado correctamente");
-			} else {
-				System.err.println("No se ha insertado correctamente");
-				System.err.println(numeroRegistrosModificados);
+				ps.setString(1, titulo);
+				ps.setString(2, isbn);
+				ps.setInt(3, numeroPaginas);
+				ps.setBoolean(4, formato);
+				int numeroRegistrosModificados = ps.executeUpdate();
+
+				if (numeroRegistrosModificados == 1) {
+					System.out.println("Insertado correctamente");
+				} else {
+					System.err.println("No se ha insertado correctamente");
+					System.err.println(numeroRegistrosModificados);
+				}
 			}
+
 		} catch (SQLException e) {
-			throw new AccesoDatosException("No se ha podido insertar el libro");
+			throw new AccesoDatosException("Error al insertar el libro", e);
 		}
-		
+
 	}
-	
+
 	private static void update() {
 		System.out.println("Conectando para actualizar el libro");
 
-		try (Connection con = DriverManager.getConnection(URL, usuario, password)){
+		try (Connection con = getConnection()) {
 			System.out.println("Conectado");
-			Long idAModificar = 1L;
-			String titulo= "La roca 2";
-			String isbn="9638527410";
-			String numeroPaginas="2000";
-			String formato="papel";
+			Long idAModificar = 7L;
+			String titulo = "cambiado";
+			String isbn = "0000000000";
+			Integer numeroPaginas = 223;
+			Boolean formato = false;
 			String sql = "UPDATE libros SET titulo=?, isbn=?, numeroPaginas=?, formato =? WHERE id=?";
-		
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, titulo);
-			ps.setString(2, isbn);
-			ps.setString(3, numeroPaginas);
-			ps.setString(4, formato);
-			ps.setLong(5, idAModificar);
-			int numeroRegistrosmodificados=ps.executeUpdate();
-			if (numeroRegistrosmodificados==1) {
-				System.out.println("Modificado correctamente");
-			}else{
-				System.out.println("No modificado correctamente");
+
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
+
+				ps.setString(1, titulo);
+				ps.setString(2, isbn);
+				ps.setInt(3, numeroPaginas);
+				ps.setBoolean(4, formato);
+				ps.setLong(5, idAModificar);
+				int numeroRegistrosmodificados = ps.executeUpdate();
+				if (numeroRegistrosmodificados == 1) {
+					System.out.println("Modificado correctamente");
+				} else {
+					System.out.println("No modificado correctamente");
+				}
 			}
 		} catch (SQLException e) {
-				throw new AccesoDatosException("No se ha podido modificar el libro ");
+			throw new AccesoDatosException("No se ha podido modificar el libro ", e);
 		}
 
 	}
@@ -194,204 +209,27 @@ public class PresentacionConsola {
 	private static void delete() {
 		System.out.println("Conectando para eliminar libro");
 
-		try (Connection con = DriverManager.getConnection(URL, usuario, password)) {
+		try (Connection con = getConnection()) {
 			System.out.println("Conectado");
-			Long id = 1L;
+			Long id = 8L;
 			String sql = "DELETE FROM libros WHERE id = ?";
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setLong(1, id);
-			int numeroRegistrosModificados = ps.executeUpdate();
-			if (numeroRegistrosModificados == 1) {
-				System.out.println("Se ha eliminado correctamente el libro");
-			} else {
-				System.err.println("No se ha eliminado correctamente");
-				System.err.println(numeroRegistrosModificados);
-			}
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
 
+				ps.setLong(1, id);
+				int numeroRegistrosModificados = ps.executeUpdate();
+				if (numeroRegistrosModificados == 1) {
+					System.out.println("Se ha eliminado correctamente el libro");
+				} else {
+					System.err.println("No se ha eliminado correctamente");
+					System.err.println(numeroRegistrosModificados);
+				}
+			}
 		} catch (SQLException e) {
-			throw new AccesoDatosException("No se ha podido eliminar el libro");
+			throw new AccesoDatosException("No se ha podido eliminar el libro", e);
 		}
 	}
 
 	private static void saludoFinal() {
-		System.out.println("Saliendo. Gracias por utilizar la apliación.");
+		System.out.println("Saliendo. Gracias por utilizar la aplicación.");
 	}
 }
-/*
- * boolean esNuevoRegistro;
- *
- *
- * do {
- * 
- * System.out.println("Conectando");
- * 
- * Connection con = DriverManager.getConnection(URL, usuario, password);
- * 
- * System.out.println("Conectado");
- * 
- * System.out.println("LISTADO DE LIBROS");
- * 
- * String sql = "SELECT * FROM libros";
- * 
- * Statement s = con.createStatement();
- * 
- * ResultSet rs = s.executeQuery(sql);
- * 
- * while (rs.next()) { System.out.println(rs.getString("titulo") + "\t" +
- * rs.getString("isbn") + "\t" + rs.getInt("numeroPaginas") + "\t" +
- * rs.getString("formato")); }
- * 
- * System.out.println("AGREGAR LIBROS");
- * 
- * String titulo = "La montaña"; String isbn = "1234567890"; String
- * numeroPaginas = "111"; String formato = "digital";
- * 
- * sql =
- * "INSERT INTO libros (titulo, isbn, numeroPaginas,formato) VALUES (?,?,?,?) ";
- * PreparedStatement ps = con.prepareStatement(sql);
- * 
- * ps.setString(1, titulo); ps.setString(2, isbn); ps.setString(3,
- * numeroPaginas); ps.setString(4, formato);
- * 
- * int numeroRegistrosModificados = ps.executeUpdate();
- * 
- * if (numeroRegistrosModificados == 1) {
- * System.out.println("Insertado correctamente"); } else {
- * System.err.println("No se ha insertado correctamente");
- * System.err.println(numeroRegistrosModificados); }
- * 
- * System.out.println("MODIFICAR LIBRO"); Long idAModificar = 3L; titulo =
- * "El sendero"; isbn = "0987654321"; numeroPaginas = "66"; formato = "papel";
- * 
- * sql =
- * "UPDATE usuarios SET titulo = ?, isbn = ?, numeroPaginas = ?, formato = ? WHERE id = ?"
- * ;
- * 
- * ps = con.prepareStatement(sql);
- * 
- * ps.setString(1, titulo); ps.setString(2, isbn); ps.setString(3,
- * numeroPaginas); ps.setString(4, formato); ps.setLong(5, idAModificar);
- * 
- * numeroRegistrosModificados = ps.executeUpdate();
- * 
- * if (numeroRegistrosModificados == 1) {
- * System.out.println("Libro modificado correctamente"); } else {
- * System.err.println("No se ha modificado correctamente");
- * System.err.println(numeroRegistrosModificados); }
- * 
- * 
- * System.out.println("ELIMINAR LIBRO"); Long id = 2L; sql =
- * "DELETE FROM libros WHERE id = ?";
- * 
- * ps = con.prepareStatement(sql);
- * 
- * ps.setLong(1, id); numeroRegistrosModificados = ps.executeUpdate();
- * 
- * if(numeroRegistrosModificados == 1) {
- * System.out.println("El libro se ha borrado correctamente"); } else {
- * System.err.println("No se ha borrado correctamente");
- * System.err.println(numeroRegistrosModificados); }
- * 
- * 
- * ps.close(); rs.close(); s.close(); con.close();
- * 
- * listadoLibros();
- * 
- * Libro libro = pedirLibro();
- * 
- * mostrarLibro(libro);
- * 
- * guardarLibro(libro);
- * 
- * 
- * // Preguntar si el usuario quiere introducir un nuevo registro
- * System.out.print("�Desea introducir un nuevo registro? (S/n) ");
- * esNuevoRegistro = !scanner.nextLine().equalsIgnoreCase("n");
- * }while(esNuevoRegistro);
- * 
- * System.out.
- * println("Saliendo de la aplicaci�n. Gracias por usar esta aplicaci�n");
- * 
- * scanner.close(); }
- * 
- * private static Libro pedirLibro() {
- * 
- * // Pedir datos de libro String titulo, isbn; int numeroPaginas = -1; boolean
- * formato = false;
- * 
- * titulo = pedirTitulo(); isbn = pedirIsbn(); numeroPaginas =
- * pedirNumeroPaginas(numeroPaginas); formato = pedirFormato(formato);
- * 
- * // Creaci�n de libro en base a los datos introducidos Libro libro = new
- * Libro(ultimoId, titulo, isbn, numeroPaginas, formato);
- * 
- * return libro; }
- * 
- * private static void guardarLibro(Libro libro) {
- * System.out.print("�Quieres guardar este libro? (S/n) ");
- * 
- * // Si me dicen que s�, guardo el libro if
- * (!scanner.nextLine().equalsIgnoreCase("n")) { // A�adir el libro a la
- * colecci�n de libros libros.put(ultimoId++, libro); } }
- * 
- * private static void listadoLibros() {
- * System.out.println("LISTADO DE LIBROS");
- * 
- * System.out.println("Id\tISBN\t\tN�mero de p�ginas\tFormato\tT�tulo"); for
- * (Libro libroExtraido : libros.values()) {
- * System.out.println(libroExtraido.getId() + "\t" + libroExtraido.getIsbn() +
- * "\t" + libroExtraido.getNumeroPaginas() + "\t\t" + (libroExtraido.isFormato()
- * ? "Digital" : "Papel") + "\t" + libroExtraido.getTitulo()); } }
- * 
- * private static void mostrarLibro(Libro libro) { System.out.println("Id: " +
- * libro.getId() + "\nT�tulo: " + libro.getTitulo() + "\nISBN: " +
- * libro.getIsbn() + "\nN�mero de p�ginas: " + libro.getNumeroPaginas() +
- * "\nFormato: " + (libro.isFormato() ? "Digital" : "Papel")); }
- * 
- * private static boolean pedirFormato(boolean formato) { boolean
- * esFormatoCorrecto; String stringFormato; do { // Introducci�n de formato
- * System.out.print("Formato (digital o papel): "); stringFormato =
- * scanner.nextLine().trim();
- * 
- * if (stringFormato.equalsIgnoreCase("digital") ||
- * stringFormato.equalsIgnoreCase("papel")) { esFormatoCorrecto = true; formato
- * = stringFormato.equalsIgnoreCase("digital"); } else { esFormatoCorrecto =
- * false; System.out.println("El formato debe ser digital o papel"); } } while
- * (!esFormatoCorrecto); return formato; }
- * 
- * private static int pedirNumeroPaginas(int numeroPaginas) { boolean
- * esNumeroPaginasCorrecto; do { // Introducci�n de n�mero de p�ginas
- * System.out.print("N�mero de p�ginas: ");
- * 
- * try { numeroPaginas = scanner.nextInt();
- * 
- * if (numeroPaginas >= 1) { esNumeroPaginasCorrecto = true; } else {
- * esNumeroPaginasCorrecto = false;
- * System.out.println("El n�mero de p�ginas debe ser m�nimo 1"); } } catch
- * (Exception e) {
- * System.out.println("Debes introducir un n�mero (s�lo d�gitos)");
- * esNumeroPaginasCorrecto = false; } finally { // Consumir el enter si ha ido
- * bien, o el texto si ha habido una excepci�n scanner.nextLine(); } } while
- * (!esNumeroPaginasCorrecto); return numeroPaginas; }
- * 
- * private static String pedirIsbn() { boolean esIsbnCorrecto; String isbn; do {
- * // Introducci�n de ISBN System.out.print("ISBN: "); isbn =
- * scanner.nextLine();
- * 
- * if (isbn.matches("\\d{10}")) { esIsbnCorrecto = true; } else { esIsbnCorrecto
- * = false; System.out.println("El ISBN debe ser un n�mero de longitud 10"); } }
- * while (!esIsbnCorrecto); return isbn; }
- * 
- * private static String pedirTitulo() { boolean esTituloCorrecto; String
- * titulo; do { // Introducci�n de t�tulo System.out.print("T�tulo: "); titulo =
- * scanner.nextLine().trim();
- * 
- * // Validaci�n del t�tulo if (titulo.length() > 3 && titulo.length() <= 150) {
- * esTituloCorrecto = true;
- * 
- * } else { esTituloCorrecto = false; System.out.
- * println("El t�tulo debe tener un tama�o m�nimo de 3 letras y m�ximo de 150");
- * } } while (!esTituloCorrecto); return titulo; }
- * 
- * }
- */
