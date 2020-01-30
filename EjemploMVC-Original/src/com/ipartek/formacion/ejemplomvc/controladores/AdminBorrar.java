47b83ab8281b8d570f90af5973012f00acb45caa
iñaki alvira <i.alvira.urrutia@gmail.com>
2020-01-30 12:42:24 +0100

    Ejemplo MVC original (sin maven)

---
+++ EjemploMVC-Original/src/com/ipartek/formacion/ejemplomvc/controladores/AdminBorrar.java
@@ -0,0 +1,27 @@
+package com.ipartek.formacion.ejemplomvc.controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ipartek.formacion.ejemplomvc.repositorios.VideoTreeMap;
+
+@WebServlet("/admin/borrar")
+public class AdminBorrar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+		VideoTreeMap.getInstancia().borrar(Long.parseLong(id));
+		
+		response.sendRedirect(request.getContextPath() + "/admin/index");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
