52efeef678a6430fb05050e6e4044470f3a71d13
JoseAngel <JoseAngel@Chopin>
2020-02-13 00:05:43 +0100

    consulta select

--- SQL_BD/Aplicacion WEB Salon de masaje/consulta seleccionar id, datos cliente, datos trabajador, datos servicio, fecha, resena, calificacion CON CONCATENACION.sql
+++ SQL_BD/Aplicacion WEB Salon de masaje/consulta seleccionar id, datos cliente, datos trabajador, datos servicio, fecha, resena, calificacion CON CONCATENACION.sql
@@ -1,6 +1,5 @@
-SELECT sesion.id, CONCAT(c.idclientes, "-", c.nombre, " ", c.apellidos) as cliente , CONCAT(t.nombre, " ", t.apellidos) as trabajador, s.idservicios as servicio, sesion.fecha as fecha, sesion.resena as resena, sesion.calificacion as calificacion
+SELECT sesion.id, CONCAT(c.idclientes, "-", c.nombre, " ", c.apellidos) as cliente , CONCAT(t.idtrabajadores, "-",t.nombre, " ", t.apellidos) as trabajador, s.idservicios as servicio, sesion.fecha as fecha, sesion.resena as resena, sesion.calificacion as calificacion
 FROM sesiones sesion
 INNER JOIN clientes c ON sesion.clientes_idclientes=c.idclientes
-INNER JOIN trabajadores t ON sesion.trabajadores_idtrabajadores=t.idtrabajadores
-INNER JOIN servicios s ON sesion.servicios_idservicios=s.idservicios
-ORDER BY sesion.id;
+INNER JOIN trabajadores t ON sesion.trabajadores_idtrabajadores=t.idtrabajadores 
+INNER JOIN servicios s ON sesion.servicios_idservicios=s.idservicios;
