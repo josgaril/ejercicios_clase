ef5b0096d6f1478b8d6c522224452779ef1a7469
JoseAngel <JoseAngel@Chopin>
2020-02-28 00:18:57 +0100

    Creado ServicioWebCalculadora con su clase Calculadora(metodos sumar, restar, multiplicar y dividir) y creado service web

---
+++ ServicioWebCalculadora/src/com/ipartek/formacion/serviciowebcalculadora/Calculadora.java
@@ -0,0 +1,19 @@
+package com.ipartek.formacion.serviciowebcalculadora;
+
+public class Calculadora {
+	public int sumar(int a, int b) {
+		return a + b;
+	}
+	
+	public int restar(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiplicar(int a, int b) {
+		return a*b;
+	}
+	
+	public int dividir(int a, int b) {
+		return a/b;
+	}
+}
