efbc67df52b70867c898b7ce7e90ff89950fa9b4
JoseAngel <JoseAngel@Chopin>
2020-03-19 21:04:20 +0100

    Creados los modelos de MF0966_3EjercicioCasa

---
+++ MF0966_3EjercicioCasa/src/com/ipartek/formacion/mf0966_3ejerciciocasa/modelos/Alumno1.java
@@ -0,0 +1,119 @@
+package com.ipartek.formacion.mf0966_3ejerciciocasa.modelos;
+
+import java.util.Date;
+
+public class Alumno1 extends Persona1 {
+
+	private String apellidos;
+	private Date fNacimiento;
+	private String dni;
+	private int nHermanos;
+	private boolean activo = true;
+
+	public Alumno1(Integer codigo, String nombre, String apellidos, Date fNacimiento, String direccion, String poblacion,
+			int codigopostal, int telefono, String email, String dni, int nHermanos, boolean activo) {
+		super(codigo, nombre, direccion, poblacion, codigopostal, email, telefono);
+		setApellidos(apellidos);
+		setfNacimiento(fNacimiento);
+		setDni(dni);
+		setnHermanos(nHermanos);
+		setActivo(activo);
+	}
+
+	public Alumno1(Persona1 p, String apellidos, Date fNacimiento, String dni, int nHermanos, boolean activo) {
+		this(null, p.getNombre(), apellidos, fNacimiento, p.getDireccion(), p.getPoblacion(), p.getCodigopostal(),
+				p.getTelefono(), p.getEmail(), dni, nHermanos, activo);
+	}
+
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Date getfNacimiento() {
+		return fNacimiento;
+	}
+
+	public void setfNacimiento(Date fNacimiento) {
+		this.fNacimiento = fNacimiento;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getnHermanos() {
+		return nHermanos;
+	}
+
+	public void setnHermanos(int nHermanos) {
+		this.nHermanos = nHermanos;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (activo ? 1231 : 1237);
+		result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((fNacimiento == null) ? 0 : fNacimiento.hashCode());
+		result = prime * result + nHermanos;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alumno1 other = (Alumno1) obj;
+		if (activo != other.activo)
+			return false;
+		if (apellidos == null) {
+			if (other.apellidos != null)
+				return false;
+		} else if (!apellidos.equals(other.apellidos))
+			return false;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (fNacimiento == null) {
+			if (other.fNacimiento != null)
+				return false;
+		} else if (!fNacimiento.equals(other.fNacimiento))
+			return false;
+		if (nHermanos != other.nHermanos)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [apellidos=" + apellidos + ", fNacimiento=" + fNacimiento + ", dni=" + dni + ", nHermanos="
+				+ nHermanos + ", activo=" + activo + ", toString() = " + super.toString() + "]";
+	}
+
+}
---
+++ MF0966_3EjercicioCasa/src/com/ipartek/formacion/mf0966_3ejerciciocasa/modelos/Persona1.java
@@ -0,0 +1,129 @@
+package com.ipartek.formacion.mf0966_3ejerciciocasa.modelos;
+
+public class Persona1 {
+
+	protected Integer codigo;
+	protected String nombre;
+	protected String direccion;
+	protected String poblacion;
+	protected int codigopostal;
+	protected String email;
+	protected int telefono;
+	
+	public Persona1(Integer codigo, String nombre, String direccion, String poblacion, int codigopostal, String email,
+			int telefono) {
+		setCodigo(codigo);
+		setNombre(nombre);
+		setDireccion(direccion);
+		setPoblacion(poblacion);
+		setCodigopostal(codigopostal);
+		setEmail(email);
+		setTelefono(telefono);
+	}
+
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getPoblacion() {
+		return poblacion;
+	}
+	public void setPoblacion(String poblacion) {
+		this.poblacion = poblacion;
+	}
+	public int getCodigopostal() {
+		return codigopostal;
+	}
+	public void setCodigopostal(int codigopostal) {
+		this.codigopostal = codigopostal;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + codigopostal;
+		result = prime * result + ((direccion == null) ? 0 : direccion.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((poblacion == null) ? 0 : poblacion.hashCode());
+		result = prime * result + telefono;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona1 other = (Persona1) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (codigopostal != other.codigopostal)
+			return false;
+		if (direccion == null) {
+			if (other.direccion != null)
+				return false;
+		} else if (!direccion.equals(other.direccion))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (poblacion == null) {
+			if (other.poblacion != null)
+				return false;
+		} else if (!poblacion.equals(other.poblacion))
+			return false;
+		if (telefono != other.telefono)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Persona [codigo=" + codigo + ", nombre=" + nombre + ", direccion=" + direccion + ", poblacion="
+				+ poblacion + ", codigopostal=" + codigopostal + ", email=" + email + ", telefono=" + telefono + "]";
+	}
+	
+	
+	
+	
+}
