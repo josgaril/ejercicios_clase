d31a62d6ccaf6ea01267d187d70104b1d6eae786
JoseAngel <JoseAngel@Chopin>
2020-03-24 14:32:16 +0100

    Pasado Proyecto MF0966_3Ejercicio casa al nuevo repositorio

--- MF0966_3EjercicioCasa/WebContent/WEB-INF/vistas/plantilla.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-
-<%@ include file="/WEB-INF/vistas/includes/cabecera.jsp" %>
-
-<%@ include file="/WEB-INF/vistas/includes/pie.jsp" %>
--- MF0966_3EjercicioCasa/src/com/ipartek/formacion/mf0966_3ejerciciocasa/repositorios/CursoDao.java
+++
@@ -1,7 +0,0 @@
-package com.ipartek.formacion.mf0966_3ejerciciocasa.repositorios;
-
-import com.ipartek.formacion.mf0966_3ejerciciocasa.modelos.Curso;
-
-public interface CursoDao extends Dao<Curso>{
-	Iterable<Curso> obtenerTodosRealizados();
-}
