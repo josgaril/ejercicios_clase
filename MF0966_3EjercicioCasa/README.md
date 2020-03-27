3b313d83bc21fa455241d7e535e3e0bf7573928d
JoseAngel <JoseAngel@Chopin>
2020-03-27 13:10:55 +0100

    Mas modificaciones de carpetas

--- MF0966_3EjercicioCasa/README.md
+++
@@ -1,2 +0,0 @@
-# MF0966_3EjercicioCasa
-Repositorio creado para la actividad propuesta sobre el examen de módulo MF0966_3
--- MF0966_3EjercicioCasa/src/com/ipartek/formacion/mf0966_3ejerciciocasa/repositorios/ResenaDao.java
+++
@@ -1,7 +0,0 @@
-package com.ipartek.formacion.mf0966_3ejerciciocasa.repositorios;
-
-import com.ipartek.formacion.mf0966_3ejerciciocasa.modelos.Resena;
-
-public interface ResenaDao extends Dao<Resena>{
-	Iterable<Resena> obtenerTodasPorCodigoCurso(Integer codigo);
-}
